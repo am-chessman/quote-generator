4b3379f9405019a74492f98564d25a0fdd7dd70f
Mark Wabwire <markwabwire256@yahoo.com>
2024-04-18 16:55:39 +0200

    nothing

---
+++ script.js
@@ -0,0 +1,281 @@
+const quotesList = [{
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs"
+}, {
+    quote: "In three words I can sum up everything I've learned about life: it goes on.",
+    author: "Robert Frost"
+}, {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt"
+}, {
+    quote: "The only thing we have to fear is fear itself.",
+    author: "Franklin D. Roosevelt"
+}, {
+    quote: "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt"
+}, {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon"
+}, {
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius"
+}, {
+    quote: "The only true wisdom is in knowing you know nothing.",
+    author: "Socrates"
+}, {
+    quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    author: "Ralph Waldo Emerson"
+}, {
+    quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston Churchill"
+}, {
+    quote: "Genius is one percent inspiration and ninety-nine percent perspiration.",
+    author: "Thomas Edison"
+}, {
+    quote: "You miss 100% of the shots you don't take.",
+    author: "Wayne Gretzky"
+}, {
+    quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    author: "Thomas A. Edison"
+}, {
+    quote: "Life isn't about finding yourself. Life is about creating yourself.",
+    author: "George Bernard Shaw"
+}, {
+    quote: "Every child is an artist. The problem is how to remain an artist once he grows up.",
+    author: "Pablo Picasso"
+}, {
+    quote: "The best time to plant a tree was 20 years ago. The second best time is now.",
+    author: "Chinese Proverb"
+}, {
+    quote: "You must be the change you wish to see in the world.",
+    author: "Mahatma Gandhi"
+}, {
+    quote: "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt"
+}, {
+    quote: "The journey of a thousand miles begins with one step.",
+    author: "Lao Tzu"
+}, {
+    quote: "What you get by achieving your goals is not as important as what you become by achieving your goals.",
+    author: "Zig Ziglar"
+}, {
+    quote: "It is never too late to be what you might have been.",
+    author: "George Eliot"
+}, {
+    quote: "If you want to lift yourself up, lift up someone else.",
+    author: "Booker T. Washington"
+}, {
+    quote: "Do not go where the path may lead, go instead where there is no path and leave a trail.",
+    author: "Ralph Waldo Emerson"
+}, {
+    quote: "The only way to achieve the impossible is to believe it is possible.",
+    author: "Charles Kingsleigh"
+}, {
+    quote: "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+    author: "Aristotle"
+}, {
+    quote: "The only thing worse than being blind is having sight but no vision.",
+    author: "Helen Keller"
+}, {
+    quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela"
+}, {
+    quote: "It always seems impossible until it is done.",
+    author: "Nelson Mandela"
+}, {
+    quote: "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
+    author: "Christian D. Larson"
+}, {
+    quote: "Happiness is not something ready-made. It comes from your own actions.",
+    author: "Dalai Lama XIV"
+}, {
+    quote: "The only person you should try to be better than is the person you were yesterday.",
+    author: "Anonymous"
+}, {
+    quote: "We become what we think about.",
+    author: "Earl Nightingale"
+}, {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs"
+}, {
+    quote: "Everything you can imagine is real.",
+    author: "Pablo Picasso"
+}, {
+    quote: "What we think, we become.",
+    author: "Buddha"
+}, {
+    quote: "Whatever you are, be a good one.",
+    author: "Abraham Lincoln"
+}, {
+    quote: "You must be the change you wish to see in the world.",
+    author: "Mahatma Gandhi"
+}, {
+    quote: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein"
+}, {
+    quote: "Do what you can, with what you have, where you are.",
+    author: "Theodore Roosevelt"
+}, {
+    quote: "The best revenge is massive success.",
+    author: "Frank Sinatra"
+}, {
+    quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    author: "Ralph Waldo Emerson"
+}, {
+    quote: "The only way to predict the future is to create it.",
+    author: "Abraham Lincoln"
+}, {
+    quote: "In order to be irreplaceable one must always be different.",
+    author: "Coco Chanel"
+}, {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt"
+}, {
+    quote: "The only thing necessary for the triumph of evil is for good men to do nothing.",
+    author: "Edmund Burke"
+}, {
+    quote: "The only true wisdom is in knowing you know nothing.",
+    author: "Socrates"
+}, {
+    quote: "If you want to shine like the sun, first burn like the sun.",
+    author: "A. P. J. Abdul Kalam"
+}, {
+    quote: "What lies behind us and what lies before us are tiny matters compared to what lies within us.",
+    author: "Ralph Waldo Emerson"
+}, {
+    quote: "It's not what you look at that matters, it's what you see.",
+    author: "Henry David Thoreau"
+}, {
+    quote: "No one can make you feel inferior without your consent.",
+    author: "Eleanor Roosevelt"
+}, {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs"
+}, {
+    quote: "The only thing necessary for the triumph of evil is for good men to do nothing.",
+    author: "Edmund Burke"
+}, {
+    quote: "Happiness is not something ready-made. It comes from your own actions.",
+    author: "Dalai Lama XIV"
+}, {
+    quote: "It is never too late to be what you might have been.",
+    author: "George Eliot"
+}, {
+    quote: "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt"
+}, {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon"
+}, {
+    quote: "The journey of a thousand miles begins with one step.",
+    author: "Lao Tzu"
+}, {
+    quote: "It is during our darkest moments that we must focus to see the light.",
+    author: "Aristotle"
+}, {
+    quote: "Change your thoughts and you change your world.",
+    author: "Norman Vincent Peale"
+}, {
+    quote: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+    author: "Albert Schweitzer"
+}, {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt"
+}, {
+    quote: "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
+    author: "Thomas A. Edison"
+}, {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs"
+}, {
+    quote: "The only thing necessary for the triumph of evil is for good men to do nothing.",
+    author: "Edmund Burke"
+}, {
+    quote: "Happiness is not something ready-made. It comes from your own actions.",
+    author: "Dalai Lama XIV"
+}, {
+    quote: "It is never too late to be what you might have been.",
+    author: "George Eliot"
+}, {
+    quote: "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt"
+}, {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon"
+}, {
+    quote: "The journey of a thousand miles begins with one step.",
+    author: "Lao Tzu"
+}, {
+    quote: "It is during our darkest moments that we must focus to see the light.",
+    author: "Aristotle"
+}, {
+    quote: "Change your thoughts and you change your world.",
+    author: "Norman Vincent Peale"
+}, {
+    quote: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+    author: "Albert Schweitzer"
+}, {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt"
+}, {
+    quote: "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
+    author: "Thomas A. Edison"
+}, {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs"
+}, {
+    quote: "The only thing necessary for the triumph of evil is for good men to do nothing.",
+    author: "Edmund Burke"
+}, {
+    quote: "Happiness is not something ready-made. It comes from your own actions.",
+    author: "Dalai Lama XIV"
+}, {
+    quote: "It is never too late to be what you might have been.",
+    author: "George Eliot"
+}, {
+    quote: "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt"
+}, {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon"
+}, {
+    quote: "The journey of a thousand miles begins with one step.",
+    author: "Lao Tzu"
+}, {
+    quote: "It is during our darkest moments that we must focus to see the light.",
+    author: "Aristotle"
+}, {
+    quote: "Change your thoughts and you change your world.",
+    author: "Norman Vincent Peale"
+}, {
+    quote: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+    author: "Albert Schweitzer"
+}, {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt"
+}, {
+    quote: "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
+    author: "Thomas A. Edison"
+}, // Add more quotes here...
+];
+
+let quoteElement = '';
+let authorElement = '';
+
+quoteElement = document.querySelector('.quote').innerHTML
+authorElement = document.querySelector('.author')
+
+function randomQuote() {
+    let quoteChoice = quotesList[Math.floor(Math.random() * quotesList.length)]
+
+    quoteElement = quoteChoice.quote
+    authorElement = quoteChoice.author
+
+    document.querySelector('.quote').innerHTML = quoteElement
+    document.querySelector('.author').innerHTML = authorElement
+
+}
+
+const quoteSelector = document.querySelector('.quote')
+
+quoteSelector.classList.add('q-mark')
