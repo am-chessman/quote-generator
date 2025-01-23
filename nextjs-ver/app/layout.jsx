61fb3d837974d7e6237975ffa9515d929938db8e
chessman <mark.chessman@yahoo.com>
2025-01-23 22:40:51 +0100

    Fix favicon setup and update dark mode handling in Next.js app

--- nextjs-ver/app/layout.jsx
+++ nextjs-ver/app/layout.jsx
@@ -8,6 +8,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/ico" sizes="32x32" href="favicon-32x32.ico"/>
+      </head>
       <body>
         {children}
       </body>
