beb7976c7afda1223dbb8d58d4710da06c17b08f
chessman <mark.chessman@yahoo.com>
2025-01-23 23:41:21 +0100

    Refactor project structure: update package name, remove unused files, and add new SVG assets

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,3 @@
-import "./globals.css"
-
 export const metadata = {
   title: "Quotes",
   description: "Most famous quotes",
@@ -14,8 +12,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&display=swap" rel="stylesheet" />
-        <link rel="icon" type="image/ico" sizes="32x32" href="favicon-32x32.ico"/>
-        <link rel="manifest" href="site.webmanifest"/>
+        <link rel="icon" type="image/ico" sizes="32x32" href="favicon.ico"/>
       </head>
       <body>
         {children}
